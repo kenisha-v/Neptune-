4a53f21591c571d630ad3458d6ffa7afd4571bcc
Rachit Gupta <rachitgupta@ucsb.edu>
2023-10-11 23:45:06 -0700

    Error handling included for lex.h

--- src/lib/lex.h
+++ src/lib/lex.h
@@ -1,9 +1,9 @@
 #ifndef LEX_H
 #define LEX_H
 
-#include <iostream>
 #include <vector>
 #include <string>
+#include <exception>
 
 enum class TokenType {
     NUMBER,
@@ -20,8 +20,26 @@ struct token {
     TokenType   type;
 };
 
+class SyntaxError : public std::exception {
+public:
+    int row;
+    int col;
+    std::string message;
+    
+    SyntaxError(int r, int c){
+        row = r;
+        col = c;
+        message = "Syntax error at line " + std::to_string(row) + ", column " + std::to_string(col) + ".";
+    }
+
+    const char* what() const noexcept override {
+        return message.c_str();
+    }
+};
+
+
 std::vector<token> tokenize(std::string input);
+// This function will "THROW SyntaxError" on encountering invalid characters.
+// Requesting Parser team to try-catch SytaxError while calling tokenize, and elemintate parser execution if error is already caught during lexer.
 
 #endif // LEX_H
-
-
