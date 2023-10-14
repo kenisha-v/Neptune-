004b3e053e23a595a13a53294712398f932ab3c4
Zeel Patel <zpatel@ucsb.edu>
2023-10-13 17:04:11 -0700

    AST and Print

---
+++ src/lib/parse.h
@@ -0,0 +1,32 @@
+#ifndef PARSE_H
+#define PARSE_H
+
+#include "lex.h"
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+struct Node {
+    std::string text;
+    Node* l_child;
+    Node* r_child;
+    Node();
+    Node(std::string text, Node* l_child = nullptr, Node* r_child = nullptr);
+};
+
+class AST{
+    public:
+        Node* main;
+        int position_n;
+        AST();
+        void construct(std::vector<token> tokenized);
+        void print(Node* main);
+    private:
+        Node* create(std::vector<token> tokenized, int position = 0);
+};
+
+
+#endif
---
+++ src/parse.cpp
@@ -0,0 +1,70 @@
+#include "lib/parse.h"
+
+Node::Node() {
+    text="";
+    l_child = r_child = nullptr;
+}
+
+Node::Node(std::string text, Node* l_child, Node* r_child) {
+    this->text = text;
+    this->l_child = l_child;
+    this->r_child = r_child;
+}
+
+AST::AST() {
+    this->main = nullptr;
+}
+
+void AST::construct(std::vector<token> tokenized){
+    this->main = create(tokenized);
+}
+
+Node* AST::create(std::vector<token> tokenized, int position){
+    if ((size_t)position>tokenized.size()){
+        return nullptr;
+    }
+    std::string value = tokenized[position].text;
+    while (value=="(" || value==")") {
+        ++position;
+        value = tokenized[position].text;
+    }
+    position_n = position+1;
+    while (tokenized[position_n].text=="(" || tokenized[position_n].text==")") {
+        ++position_n;
+    }
+    if (value=="+" || value=="-" || value=="*"  || value=="/") {
+        Node* new_main = new Node(value);
+        new_main->l_child = create(tokenized, position_n);
+        new_main->r_child = create(tokenized, position_n);
+        return new_main;
+    } 
+    else if (value=="0" || value=="1" || value=="2" || value=="3" || value=="4" || 
+    value=="5" || value=="6" || value=="7" || value=="8" || value=="9") {
+        Node* new_main = new Node(value);
+        return new_main;
+    }
+    else {
+        return nullptr;
+    }
+}
+
+void AST::print(Node* main) {
+    if (main->l_child==nullptr && main->r_child==nullptr) {
+        std::cout << main->text;
+    }
+    else {
+        if (main->l_child!=nullptr) {
+            std::cout << "(";
+            print(main->l_child);
+            std::cout << " ";
+        }
+        if (main!=nullptr) {
+            std::cout << main->text;
+        }
+        if (main->r_child!=nullptr) {
+            std::cout << " ";
+            print(main->r_child);
+            std::cout << ")";
+        }
+    }
+}
