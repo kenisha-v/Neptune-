ff809c643770a52bf64f8bf4a1bcfc0e2bb5114b
rachit182 <rachitgupta1824@gmail.com>
2023-10-15 23:57:03 -0700

    Revert "FULLY FUNCTIONING LEXER (GRADESCOPE APPROVED)"
    This reverts commit 6dd80b2a154eb9158e43674a0ceef1326c20eda3 which was the last clean & working lexer code on main branch.

--- src/lex.cpp
+++ src/lex.cpp
@@ -38,17 +38,17 @@ void printTokens(const std::vector<token>& tokens) {
 std::vector<token> tokenize(const std::string& input) {
     vector<token> all_tokens;
     std::istringstream stream(input);
-    char ar_op[8] = {'+', '-', '*', '/', '(', ')', ' ', '\t'}; //Helps us check for valid inputs
+    char ar_op[8] = {'+', '-', '*', '/', '(', ')', ' ', '\t'};
     int row = 1;
     int col = 1;
-    std::string temp_str_num = "";// Tracks a number value under process
+    std::string temp_str_num = "";
     char in_char;
     bool hasDecimal = false; // Track if current number being processed has a decimal
 
     while (stream.get(in_char)) {
 
         if (in_char == '\n') {
-            if (not temp_str_num.empty()){ // If there's a number accumulated in temp_str_num, processing it first
+            if (not temp_str_num.empty()){
                 all_tokens.push_back(getToken(row, col, temp_str_num, TokenType::NUMBER));
                 col += temp_str_num.length();
                 temp_str_num = "";
@@ -59,24 +59,26 @@ std::vector<token> tokenize(const std::string& input) {
             continue;
         }
         else if (std::find(std::begin(ar_op), std::end(ar_op), in_char) != std::end(ar_op)) {
-            if (!temp_str_num.empty()) { // If there's a number accumulated in temp_str_num, processing it first
+            // If there's a number accumulated in temp_str_num, processing it first
+            if (!temp_str_num.empty()) {
                 all_tokens.push_back(getToken(row, col, temp_str_num, TokenType::NUMBER));
                 col += temp_str_num.length();
                 temp_str_num = "";
                 hasDecimal = false;
             }
-            
-            if (in_char != ' ' && in_char != '\t') { // Now processing the operator
+            // Now processing the operator
+            if (in_char != ' ' && in_char != '\t') {
                 all_tokens.push_back(getToken(row, col, string(1, in_char), getType(in_char)));
                 col++;
             } else {
                 col++;
             }
         }
+
         else if (isdigit(in_char)) {
             temp_str_num += in_char;
         }
-        else if (in_char == '.') { //seperate elif for . needed to ensure all cases of decimal error are detected.
+        else if (in_char == '.') {
             if (!isdigit(stream.peek())) {
                 throw SyntaxError(row, col + temp_str_num.length()+1);
             } else if (hasDecimal || temp_str_num.empty()){
@@ -103,7 +105,7 @@ int main() {
     std::string input;
     char ch;
     
-    while (std::cin.get(ch)) { //reading input character by character to ensure pre-eof chararcter detection.
+    while (std::cin.get(ch)) {
         input += ch;
     }
 
