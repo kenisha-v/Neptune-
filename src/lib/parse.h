cf1ead493a3f432f888dd5cbb3cf8a0c5be786b2
rachit182 <rachitgupta1824@gmail.com>
2023-10-24 16:06:10 -0700

    basic debugging

--- src/lib/parse.h
+++ src/lib/parse.h
@@ -37,36 +37,4 @@ class AST{
 
 };
 
-class ParseError : public std::exception {
-public:
-    int row;
-    int col;
-    token error_token;
-    std::string message;
-    
-    ParseError(int r, int c, token t){
-        row = r;
-        col = c;
-        error_token = t;
-        message = "Unexpected token at line " + std::to_string(row) + " column " + std::to_string(col) + ": " + error_token.text;
-    }
-
-    const char* what() const noexcept override {
-        return message.c_str();
-    }
-};
-
-class EvaluationError : public std::exception {
-public:
-    std::string message;
-    
-    EvaluationError(std::string message){
-        this->message = "Runtime error: " + message;
-    }
-
-    const char* what() const noexcept override {
-        return message.c_str();
-    }
-};
-
 #endif
